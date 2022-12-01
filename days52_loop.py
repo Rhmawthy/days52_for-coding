a895a165992605eeef3b42fed7230f219dac9237
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-12-01 19:48:26 +0800

    Add files via upload

---
+++ days52_loop.py
@@ -0,0 +1,6 @@
+#program user melakukan inputan angka
+# angka tersebut akan keluar mulai dari angka terbesar ke terkecil
+
+number = int(input("enter the number"))	
+for j in range(number,0 ,-1):
+	print(j)
